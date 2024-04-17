2a90e11ba73932c50d793c89df7281408e97588b
GreenyHat <aramirezgarcia95@gmail.com>
2024-04-17 10:18:18 +0200

    contacts-store

---
+++ app/Models/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "name",
+        "phone_number"
+    ];//Aqui ponemos lo que es rellenable en la base de datos, los datos que no esten aqui no seran trasladados a la bbdd
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Models\Contact;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +27,34 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Route::get('/contacts/create', [ContactController::class, 'create']);
+//esto es para seguir la logica de Ruta/Controlador/Funcion 
+//en este caso localhost:port/contacts/create
+Route::post('/contacts/', [ContactController::class, 'store'])->name('contacts.store');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('contact', fn () => Response::view('contact'));
+
+// Route::post('/contact', function (Request $request) {
+//     $data = $request->all();
+
+//     Contact::create($data);
+
+//     // $contact = new Contact();
+//     // $contact->name = $data["name"];
+//     // $contact->phone_number = $data["phone_number"];
+//     // $contact->save(); Esto si llamamos al metodo estatico create y  sobreescribimos la variables $fillable en el Contact.php y ponemos los campos admisibles, podemos relacionarlos con 
+
+
+//     return "Contact Stored";
+// });
+
+
+    
+// Route::get('/contact', function(){
+//     return Response::view('contact');//Hay que importar de Iluminate Support Facade
+// });                                     Cuando solo es un return y no tiene logica por medio se puede reducir:
+
+
+
+//     DB::statement("INSERT INTO contacts (name, phone_number) VALUES (?,?)",[$data["name"], $data["phone_number"]]);
